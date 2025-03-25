f8ddcd655edc26ef78d20d965debe563b6a04cc8
Emanuele2336 <meregalliemanuele@itis-molinari.eu>
2025-03-25 17:48:00 +0100

    gitignore

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+/*import fetch from 'node-fetch';
 
 const url = 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=bojack&country=uk';
 const options = {
@@ -16,20 +16,41 @@ try {
 } catch (error) {
 	console.error(error);
 }
+    */
 
-const fs = require('fs');
-const TelegramBot = require('node-telegram-bot-api');
+import fs from 'fs';//perchè da problemi di compatibilità il require su  es6
+import TelegramBot from 'node-telegram-bot-api';
+const url = 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=bojack&country=uk';
+
 const conf = JSON.parse(fs.readFileSync('conf.json'));
 const token = conf.key;
 
 const bot = new TelegramBot(token, { polling: true });
 
-bot.on("message", (msg) => {
+bot.on("message", async (msg) => {
    const chatId = msg.chat.id;
    const text = msg.text;
 
    if (text === "/start") {
-      bot.sendMessage(chatId, "fornisci genere");
+      bot.sendMessage(chatId, "fornisci genere film");
+   }
+
+   if(text !== "/start"){
+    const options = {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-key': 'ad55dec050msh3b20d03711d1f1fp1fcd4fjsne2ed26002d0d',
+        'x-rapidapi-host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com'
+      }
+    };
+    
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        bot.sendMessage(chatId, result);
+    } catch (error) {
+        console.error(error);
+    }
    }
    
 });
